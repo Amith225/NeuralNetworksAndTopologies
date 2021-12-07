b7512d2bc2c55e0e75b4be74d62827dd078608a7
Amith <amitharun3@gmail.com>
2021-12-07 23:55:38 +0530

    .

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ db = DataBase.load(dataSet.TrainSets.EmnistBalanced, normalize=1, reshapeInp=(-1
 db2 = DataBase.load(dataSet.TestSets.EmnistBalanced, normalize=1, reshapeInp=(-1, 1), hotEncodeTar=True)
 
 hiddenShape = 392, 196
-nn = ArtificialNeuralNetwork(wbShape=WBShape(db.inpShape, *hiddenShape, db.tarShape),
+nn = ArtificialNeuralNetwork(wbShape=WBShape(db.inpShape[0], *hiddenShape, db.tarShape[0]),
                              wbInitializer=XavierWBInitializer(2),
                              activators=Activators(Prelu(),
                                                    ...,

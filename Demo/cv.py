5079a07c9b3125d2999ee2014d17fd7a7c8b5b02
Amith <amitharun3@gmail.com>
2022-05-05 09:23:18 +0530

    save point cv

--- Demo/cv.py
+++ Demo/cv.py
@@ -2,9 +2,17 @@ import numpy as np
 import cv2
 
 
-def drawContour(src, _contour, col, lineWidth=1):
-    srcCopy = src.copy()
+def getGrp(index, _contour):
+    same_y_indexes = np.where(_contour[:, 1] == _contour[index][1])[0]
+    e = np.sort(_contour[same_y_indexes], axis=0)
+    grp = np.split(e, np.where(np.diff(e[:, 0]) != 1)[0] + 1)
+    grp = np.array([[g[0], g[-1]] for g in grp])
+    return same_y_indexes, grp
+
+
+def drawContour(src, _contour, col=0, lineWidth=1):
     if lineWidth >= 0:
+        srcCopy = src.copy()
         for line in range(lineWidth):
             Ncontour = _contour - line
             Pcontour = _contour + line
@@ -13,23 +21,30 @@ def drawContour(src, _contour, col, lineWidth=1):
             srcCopy[Pcontour[:, 1], _contour[:, 0]] = col
             srcCopy[_contour[:, 1], Pcontour[:, 0]] = col
         return srcCopy
-    elif lineWidth in (-1, -2):
-        if lineWidth == -2: srcCopy = np.ones_like(src) * np.uint8(col)
+    elif lineWidth == -1:
+        mask = src != src
         index, maxIndex = -1, _contour.shape[0]
         indexFlags = []
+        prevGrp = None
         while (index := index + 1) < maxIndex:
             if index in indexFlags: continue
-            same_y_indexes = np.where(_contour[:, 1] == _contour[index][1])[0]
+            same_y_indexes, grp = getGrp(index, _contour)
             indexFlags.extend(same_y_indexes)
-            e = _contour[same_y_indexes]
-            e1, e2 = e.min(axis=0), e.max(axis=0)
-            if lineWidth == -1:
-                srcCopy[e1[1], e1[0]:e2[0] + 1] = col
+            if prevGrp is not None and (length := len(prevGrp) - len(grp)) > 0:
+                prevGrp = grp
+                grp_ = []
+                for i in range(length): grp_.extend([grp[i], grp[i]])
+                grp = np.array(grp_ + list(grp[length:]))
             else:
-                srcCopy[e1[1], e1[0]:e2[0] + 1] = src[e1[1], e1[0]:e2[0] + 1]
-        return srcCopy
+                prevGrp = grp
+            if len(grp) % 2 != 0: grp = np.concatenate((grp, [grp[len(grp) - 2]]))
+            for i, g in enumerate(grp[::2]):
+                a, b = g[0][0], grp[i * 2 + 1][-1][0] + 1
+                if a > b: a, b = b - 1, a + 1
+                mask[g[0][1], a:b] = True
+        return mask
     else:
-        raise ValueError("lineWidth must be >= -2")
+        raise ValueError("lineWidth must be >= -1")
 
 
 def bordering(_j, _i):
@@ -43,6 +58,7 @@ def findContour(src, _hierarchy=None, _contours=None, offset=None, edgePix=None)
     if edgePix is None: edgePix = 0
     if _hierarchy is None: _hierarchy = [[-1, -1, -1, -1]]
     if _contours is None: _contours = []  # todo
+    notEdgePIx = 255 * (not edgePix)
     edge = src == edgePix
     if np.alltrue(~edge): return _hierarchy, _contours
     start = divmod(np.argmax(edge), src.shape[0])
@@ -62,9 +78,9 @@ def findContour(src, _hierarchy=None, _contours=None, offset=None, edgePix=None)
         index += 1
         current = borders[index]
     contours = np.array(contours)
-    nextSrc = drawContour(src, contours[-1], 255 * (not edgePix), -1)
-    childSrc = drawContour(src, contours[-1], 255 * edgePix, -2)
-    print('next')
+    mask = drawContour(src, contours[-1], lineWidth=-1)
+    nextSrc, childSrc = src * ~mask + np.uint8(notEdgePIx) * mask, src * mask + np.uint8(edgePix) * ~mask
+    print('next', edgePix)
     cv2.imshow('', nextSrc), cv2.waitKey(0), cv2.destroyAllWindows(), cv2.waitKey(1)
     print('child', edgePix)
     cv2.imshow('', childSrc), cv2.waitKey(0), cv2.destroyAllWindows(), cv2.waitKey(1)
@@ -74,7 +90,7 @@ def findContour(src, _hierarchy=None, _contours=None, offset=None, edgePix=None)
     else:
         h = [-1, offset, -1, _hierarchy[offset][-1]]
         _hierarchy[offset][0] = offset + 1
-    child = findContour(childSrc, [h], edgePix=255 * (not edgePix))
-    # print(len(child[0]))
-    # hierarchy_, contours_ = findContour(nextSrc, [*_hierarchy, h], contours, offset=offset + 1)
-    # return hierarchy_, contours_
+    child = findContour(childSrc, [h], edgePix=notEdgePIx)
+    sibling = findContour(nextSrc, [*_hierarchy, *child[0]], contours, offset=offset + len(child[0]) - 1, edgePix=edgePix)
+    print(sibling[0])
+    return sibling[0], contours
